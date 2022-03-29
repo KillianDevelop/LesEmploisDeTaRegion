b24c45d030f42e9d200aad026da44e6c046ded20
KillianDevelop <tznadoche@gmail.com>
2022-03-29 16:41:20 +0200

    Ajout du menu accueil

--- controleur/c_accueil.php
+++ controleur/c_accueil.php
@@ -14,7 +14,21 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
 
 include_once "$racine/modele/authentification.inc.php";
 
+
+$infosUtilisateur = getInfosUtilisateurByUuid($_SESSION["idUtil"]);
+
+if (
+    isset($infosUtilisateur["sommaireUtilisateur"]) && isset($infosUtilisateur["entrepriseUtilisateur"])
+    && isset($infosUtilisateur["posteUtilisateur"])
+) {
+    $sommaireUtilisateur = $infosUtilisateur["sommaireUtilisateur"];
+    $entrepriseUtilisateur = $infosUtilisateur["entrepriseUtilisateur"];
+    $posteUtilisateur = $infosUtilisateur["posteUtilisateur"];
+}
+
+
 if (estConnecte()) {
+    include_once "$racine/vue/header.php";
     include_once "$racine/vue/v_accueil.php";
     include_once "$racine/vue/footer.php";
 }else{
--- vue/v_accueil.php
+++ vue/v_accueil.php
@@ -1,5 +1,49 @@
-<?php
+<script src="https://kit.fontawesome.com/60856e5448.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+<link rel="stylesheet" href="css/accueil.css">
 
-include "$racine/vue/header.php";
+<div class="parent">
+    <div class="profil">
+        <div class="iconeprofil">
+            <i class="fa-solid fa-circle-user" ></i>
+        </div>
+        <p class="nom"><?php echo $_SESSION["prenomUtil"] . " " . $_SESSION["nomUtil"]; ?></p>
+        <hr class="hrprofil">
+        <p class="corpsMessage"><?php if (isset($sommaireUtilisateur)) {
+                                    echo $sommaireUtilisateur;
+                                } else {
+                                    echo "Complétez votre profil en cliquant sur le stylo au-dessus de votre 
+                                    icône utilisateur dans l'onglet <b>Mon Profil</b>.";
+                                } ?></p>
+    </div>
+    <div class="post">
+        <form input="POST">
+            <div class="englobePost">
 
-?>
+                <div class="iconePost">
+                    <i class="fa-solid fa-circle-user"></i>
+                </div>
+                <textarea name="publication" cols="40" rows="3" placeholder="Commencer un post" required="required"></textarea>
+            </div>
+
+            <button type="submit">Envoyer</button>
+
+        </form>
+    </div>
+    <div class="corps">
+        <div class="englobeInfos">
+            <div class="iconePost">
+                <i class="fa-solid fa-circle-user"></i>
+            </div>
+            <div class="infos">
+                <p class="nomCorps">Jean Pierre</p>
+                <p class="emploi"> Etudiant </p>
+                <p class="datePublication">17h</p>
+            </div>
+        </div>
+        <hr class="hrpublication">
+
+        <p class="corpsMessage">Je suis actuellement à la recherche d'un stage de trois mois pour valider ma formation </p>
+
+    </div>
+</div>
