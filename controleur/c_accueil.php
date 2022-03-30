20612a48665bdfa4f0f470170fbc9ca82cd9e9f3
KillianDevelop <tznadoche@gmail.com>
2022-03-30 20:06:22 +0200

    Ajout du feed de l'accueil

--- controleur/c_accueil.php
+++ controleur/c_accueil.php
@@ -13,26 +13,40 @@ if ($_SERVER["SCRIPT_FILENAME"] == __FILE__) {
 }
 
 include_once "$racine/modele/authentification.inc.php";
-
-
-$infosUtilisateur = getInfosUtilisateurByUuid($_SESSION["idUtil"]);
-
-if (
-    isset($infosUtilisateur["sommaireUtilisateur"]) && isset($infosUtilisateur["entrepriseUtilisateur"])
-    && isset($infosUtilisateur["posteUtilisateur"])
-) {
-    $sommaireUtilisateur = $infosUtilisateur["sommaireUtilisateur"];
-    $entrepriseUtilisateur = $infosUtilisateur["entrepriseUtilisateur"];
-    $posteUtilisateur = $infosUtilisateur["posteUtilisateur"];
-}
-
-
+include_once "$racine/modele/bd.utilisateur.inc.php";
 if (estConnecte()) {
+
+    $infosUtilisateur = getInfosUtilisateurByUuid($_SESSION["idUtil"]);
+
+    if (
+        isset($infosUtilisateur["sommaireUtilisateur"]) && isset($infosUtilisateur["entrepriseUtilisateur"])
+        && isset($infosUtilisateur["posteUtilisateur"])
+    ) {
+        $sommaireUtilisateur = $infosUtilisateur["sommaireUtilisateur"];
+        $entrepriseUtilisateur = $infosUtilisateur["entrepriseUtilisateur"];
+        $posteUtilisateur = $infosUtilisateur["posteUtilisateur"];
+
+        $message = getPublication();
+
+        if (isset($_POST["publication"]) && !empty($_POST["publication"])) {
+
+            if (strlen($_POST["publication"]) <= 200) {
+
+                filter_has_var(INPUT_POST, 'publication');
+                $publication = htmlspecialchars($_POST['publication'], ENT_NOQUOTES);
+
+                newPublication($publication, $_SESSION["idUtil"]);
+            } else {
+                $msgErreur = "Erreur, votre publication dépasse 200 caractères";
+            }
+        }
+    }
+
+
     include_once "$racine/vue/header.php";
     include_once "$racine/vue/v_accueil.php";
-    include_once "$racine/vue/footer.php";
-}else{
+    include_once "$racine/vue/footerScroll.php";
+    
+} else {
     include_once "$racine/controleur/c_authentification.php";
 }
-
-?>
